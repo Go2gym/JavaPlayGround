e9b08f893a096d1414cb34db4bd2330e821fdd96
BubblingSODA <go2gym365@gmail.com>
2023-08-15 05:38:44 +0000

    JAva

--- JavaManagementProgram/Main.java
+++ JavaManagementProgram/Main.java
@@ -2,13 +2,25 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws Exception{
-        System.out.println("원하는 구구단 단수를 입력하세요 : ");
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(buffer.readLine());
+        public static int[] gugudan(int n) {
+        int [] Result = new int [9];
+        for(int i = 0; i < Result.length; i++) {
+            Result[i] = (i + 1) * n;
+        }
+        return Result;
+    }
 
-        for(int i = 0; i < 10; i++) {
-            System.out.println(i * num);
+    public static void main(String[] args) throws Exception{
+        //System.out.print("원하는 구구단 단수를 입력하세요 : ");
+        //BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        //int num = Integer.parseInt(buffer.readLine());
+
+        for(int i = 2; i < 10; i++) {
+            int [] ans = gugudan(i);
+            System.out.printf("%d단\n", i);
+            for(int j = 0; j < 9; j++){
+                System.out.printf("%d * %d = %d\n",i , j+1, ans[j]);
+            }
         }
     }
 }
