ffa68c2a58b556df46eb4bafa2e69f16c02d6bb1
BubblingSODA <go2gym365@gmail.com>
2023-08-17 10:43:48 +0000

    Calendar

--- JavaManagementProgram/ Calendar/Calendar.java
+++ JavaManagementProgram/ Calendar/Calendar.java
@@ -4,13 +4,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Calendar {
+    static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
     public static void main(String[] args) throws IOException {
-        int a, b;
+        int a;
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-        System.out.printf("두 수의 합은 %d입니다.", a+b);
+
+        System.out.printf("%d월은 %d일까지 있습니다.", a, days[a - 1]);
+
     }
 }
