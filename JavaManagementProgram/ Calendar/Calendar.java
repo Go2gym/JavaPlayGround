0d1208d893c75d27545089ec81c09e2cc057c100
BubblingSODA <go2gym365@gmail.com>
2023-08-17 13:48:44 +0000

    Success

--- JavaManagementProgram/ Calendar/Calendar.java
+++ JavaManagementProgram/ Calendar/Calendar.java
@@ -1,45 +1,81 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Calendar {
     private final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+    private final String[] startDays = {"SU", "MO", "WE", "TH", "FR", "SA"};
+    private String [] calendar = new String[50];
     //몇일까지 있는지 출력
     public int maxDaysOfMonth(int month) {
         return Max_Days[month - 1];
     }
 
-    public void printCalendar(int year, int month) {
+    public void makeCalendar(int month, String startDay) {
+        if (Max_Days[month - 1] == 28) {
+            for(int i = 0; i < 28 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if(i < Arrays.asList(startDays).indexOf(startDay)) {
+                    calendar[i] = "";
+                }
+                else {
+                    calendar[i] = Integer.toString(i - Arrays.asList(startDays).indexOf(startDay) + 1);
+                }
+            }
+        }
+        else if (Max_Days[month - 1] == 30) {
+            for(int i = 0; i < 30 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if(i < Arrays.asList(startDays).indexOf(startDay)) {
+                    calendar[i] = "";
+                }
+                else {
+                    calendar[i] = Integer.toString(i - Arrays.asList(startDays).indexOf(startDay) + 1);
+                }
+            }
+        }
+        else if (Max_Days[month - 1] == 31) {
+            for(int i = 0; i < 31 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if(i < Arrays.asList(startDays).indexOf(startDay)) {
+                    calendar[i] = "";
+                }
+                else {
+                    calendar[i] = Integer.toString(i - Arrays.asList(startDays).indexOf(startDay) + 1);
+                }
+            }
+        }
+    }
+
+    public void printCalendar(int year, int month, String startDay) {
+        makeCalendar(month, startDay);
         if (Max_Days[month - 1] == 28) {
             System.out.printf("<<%4d년 %3d월>>\n", year, month);
             System.out.println("SU MO TU WE TH FR SA");
             System.out.println("--------------------");
-            for(int i = 1; i <= 28; i++) {
-                if(i % 7 == 0)
-                    System.out.printf("%2d\n", i);
+            for(int i = 0; i < 28 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if((i + 1) % 7 == 0)
+                    System.out.printf("%2s\n", calendar[i]);
                 else
-                    System.out.printf("%2d ", i);
+                    System.out.printf("%2s ", calendar[i]);
             }
         }
         else if (Max_Days[month - 1] == 30) {
             System.out.printf("<<%4d년 %3d월>>\n", year, month);
             System.out.println("SU MO TU WE TH FR SA");
             System.out.println("--------------------");
-            for(int i = 1; i <= 30; i++) {
-                if(i % 7 == 0)
-                    System.out.printf("%2d\n", i);
+            for(int i = 0; i < 30 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if((i + 1) % 7 == 0)
+                    System.out.printf("%2s\n", calendar[i]);
                 else
-                    System.out.printf("%2d ", i);
+                    System.out.printf("%2s ", i);
             }
         }
         else if (Max_Days[month - 1] == 31) {
             System.out.printf("<<%4d년 %3d월>>\n", year, month);
             System.out.println("SU MO TU WE TH FR SA");
             System.out.println("--------------------");
-            for(int i = 1; i <= 31; i++) {
-                if(i % 7 == 0)
-                    System.out.printf("%2d\n", i);
+            for(int i = 0; i < 31 + Arrays.asList(startDays).indexOf(startDay); i++) {
+                if((i + 1) % 7 == 0)
+                    System.out.printf("%2s\n", calendar[i]);
                 else
-                    System.out.printf("%2d ", i);
+                    System.out.printf("%2s ", calendar[i]);
             }
         }
     }
--- JavaManagementProgram/ Calendar/Prompt.java
+++ JavaManagementProgram/ Calendar/Prompt.java
@@ -1,25 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Prompt {
     private final static String PROMPT="cal> ";
 
     public void runPrompt() throws IOException{
         int month = 1;
+        int year = 1;
+        String startDay;
         //String PROMPT = "cal> ";
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         Calendar cal = new Calendar();
 
         while (month != -1) {
+            System.out.println("년도를 입력하세요");
+            System.out.print("YEAR> ");
+            year = Integer.parseInt(bf.readLine());
+
             System.out.println("달을 입력하세요");
-            System.out.print(PROMPT);
+            System.out.print("MONTH> ");
+            month = Integer.parseInt(bf.readLine());
 
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            month = Integer.parseInt(st.nextToken());
-
+            System.out.println("시작하는 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
+            System.out.print("STARTDAY> ");
+            startDay = bf.readLine();
+            
             if (month == -1) {
                 System.out.println("Bye~");
                 break;
@@ -29,7 +36,7 @@ public class Prompt {
                 System.out.println("잘못된 값입니다. 다시 입력해주세요\n");
             } else {
                 System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.maxDaysOfMonth(month));
-                cal.printCalendar(2023, month);
+                cal.printCalendar(year, month, startDay);
                 System.out.println();
             }
         }
