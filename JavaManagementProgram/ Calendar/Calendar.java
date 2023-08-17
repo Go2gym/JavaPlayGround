a2876a16b66de03e8cea3ecbd810f6a612f2ddb7
BubblingSODA <go2gym365@gmail.com>
2023-08-17 10:48:46 +0000

    calk

--- JavaManagementProgram/ Calendar/Calendar.java
+++ JavaManagementProgram/ Calendar/Calendar.java
@@ -4,16 +4,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Calendar {
-    static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+    private final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    public int maxDaysOfMonth(int n) {
+        return Max_Days[n - 1];
+    }
     public static void main(String[] args) throws IOException {
         int a;
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        Calendar cal = new Calendar();
 
         a = Integer.parseInt(st.nextToken());
 
-        System.out.printf("%d월은 %d일까지 있습니다.", a, days[a - 1]);
+        System.out.printf("%d월은 %d일까지 있습니다.", a, cal.maxDaysOfMonth(a));
 
     }
 }
