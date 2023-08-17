c0d579e48a8bc3e1ab81acf5b4fd9c20d8bd3075
BubblingSODA <go2gym365@gmail.com>
2023-08-17 11:06:08 +0000

    Calendar2

--- JavaManagementProgram/ Calendar/Calendar.java
+++ JavaManagementProgram/ Calendar/Calendar.java
@@ -5,19 +5,37 @@ import java.util.StringTokenizer;
 
 public class Calendar {
     private final int[] Max_Days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    public int maxDaysOfMonth(int n) {
-        return Max_Days[n - 1];
+
+    public int maxDaysOfMonth(int month) {
+        return Max_Days[month - 1];
     }
+
     public static void main(String[] args) throws IOException {
-        int a;
+        int month;
+        String PROMPT = "cal> ";
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
+
         Calendar cal = new Calendar();
+        
+        month = 1;
 
-        a = Integer.parseInt(st.nextToken());
+        while (month != -1) {
+            System.out.println("달을 입력하세요");
+            System.out.print(PROMPT);
+            StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        System.out.printf("%d월은 %d일까지 있습니다.", a, cal.maxDaysOfMonth(a));
-
+            month = Integer.parseInt(st.nextToken());
+            if(month == -1) {
+                System.out.println("Bye~");
+                break;
+            }
+            
+            if (month > 12 || month < 1) {
+                System.out.println("잘못된 값입니다. 다시 입력해주세요\n");
+            } else {
+                System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfMonth(month));
+                System.out.println();
+            }
+        }
     }
 }
